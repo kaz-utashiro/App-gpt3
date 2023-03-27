624cf489b393bd9ca1742c44b6b9bc2deae996d9
Kazumasa Utashiro <kaz@utashiro.com>
2023-03-27 13:48:45 +0900

    use poetry as a package manager

--- gpty/gpty.py
+++ gpty/gpty.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 """
-GPT-3 API Command Line Wrapper
+OpenAI GPT API Command Line Wrapper
 
-This Python script allows you to easily interact with the GPT-3 API from the command line.
+This Python script allows you to easily interact with the GPT API from the command line.
 You can provide various parameters as command-line options and get the generated text in the output.
 
 Usage:
-./gpt3 [prompts] [options]
+gpty [prompts] [options]
 
 Arguments:
-prompts: A list of input prompts for GPT-3, separated by spaces. If '-' is present, it will read from stdin.
+prompts: A list of input prompts for GPT, separated by spaces. If '-' is present, it will read from stdin.
 The prompts will be concatenated with newline characters.
 
 Options:
--e, --engine: The OpenAI GPT-3 engine to use (default: gpt-3.5-turbo)
+-e, --engine: The OpenAI GPT engine to use (default: gpt-3.5-turbo)
 -m, --max-tokens: Maximum number of tokens in the response (default: 100)
 -t, --temperature: Sampling temperature for randomness (default: 0.5)
 -d, --debug: Show the full response and request parameters in JSON format, with Unicode characters preserved (default: False)
@@ -33,11 +33,11 @@ import sys
 import json
 
 def cli():
-    parser = argparse.ArgumentParser(description="GPT-3 API command line wrapper")
+    parser = argparse.ArgumentParser(description="OpenAI GPT API command line wrapper")
 
-    parser.add_argument("prompts", nargs="*", type=str, help="Input prompts for GPT-3 or '-' to read from stdin")
-    parser.add_argument("-e", "--engine", type=str, default="gpt-3.5-turbo", help="OpenAI GPT-3 engine (default: gpt-3.5-turbo)")
-    parser.add_argument("-m", "--max-tokens", type=int, default=100, help="Maximum number of tokens in the response (default: 100)")
+    parser.add_argument("prompts", nargs="*", type=str, help="Input prompts for GPT or '-' to read from stdin")
+    parser.add_argument("-e", "--engine", type=str, default="gpt-3.5-turbo", help="OpenAI GPT engine (default: gpt-3.5-turbo)")
+    parser.add_argument("-m", "--max-tokens", type=int, default=2000, help="Maximum number of tokens in the response (default: 100)")
     parser.add_argument("-t", "--temperature", type=float, default=0.5, help="Sampling temperature for randomness (default: 0.5)")
     parser.add_argument("-d", "--debug", action="store_true", help="Show the request parameters and the full response in JSON format (default: False)")
     parser.add_argument("-k", "--key", type=str, default=None, help="OpenAI API key (optional, overrides OPENAI_API_KEY environment variable)")
@@ -57,18 +57,6 @@ def cli():
             prompt_parts.append(p)
 
     prompt = "\n".join(prompt_parts)
-
-    response = openai.ChatCompletion.create(
-        model=args.engine,
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt},
-        ],
-        max_tokens=args.max_tokens,
-        n=1,
-        stop=None,
-        temperature=args.temperature,
-    )
 
     request_params = {
         "model": args.engine,
