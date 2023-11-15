6b2bdeae9c90a946f3dfc1a2e3f604d94548e67a
Kazumasa Utashiro <kaz@utashiro.com>
2023-11-15 16:51:53 +0900

    use Typer for argument parsing

--- gpty/gpty.py
+++ gpty/gpty.py
@@ -8,145 +8,98 @@ gpty [prompts] [options]
 
 """
 
-from importlib.metadata import version
-import argparse
+import typer
+from typing import List, Optional
 import os
 import sys
 import re
 import json
+from importlib.metadata import version
 import click_spinner
 import openai
+
+app = typer.Typer(add_completion=False)
+
+package_name = "tecolicom-gpty"
 
 def debug_print(message, file=sys.stderr):
     print(message, file=file)
 
-package_name = "tecolicom-gpty"
+def vers(ctx: typer.Context, value: bool):
+    if not value or ctx.resilient_parsing:
+        return
+    typer.echo(f"{ctx.info_name} {version(package_name)}")
+    raise typer.Exit()
 
-def create_parser():
-
-    def args(**kwargs): return kwargs
-    spec = [
-        (("prompts",),
-         "input prompts for GPT or '-' to read from stdin", args(
-             nargs   = "*",
-             type    = str,
-         )),
-        (("-s", "--system",),
-         "set system message (can be used multiple times)", args(
-             action  = "append",
-             type    = str,
-             metavar = "MESSAGE",
-         )),
-        (("-I", "--itemize",),
-         "itemize other prompts after this message", args(
-             type    = str,
-             metavar = "MESSAGE",
-         )),
-        (("-e", "--engine",),
-         "OpenAI GPT engine", args(
-             type    = str,
-             default = "gpt-3.5-turbo",
-         )),
-        (("-m", "--max-tokens",),
-         "maximum number of tokens in the response", args(
-             type    = int,
-             default = 2000,
-         )),
-        (("-t", "--temperature",),
-         "sampling temperature for randomness", args(
-             type    = float,
-             default = 0.5,
-         )),
-        (("-k", "--key",),
-         "OpenAI API key", args(
-             type    = str,
-         )),
-        (("-q", "--squeeze",),
-         "squeeze two or more newlines into one", args(
-             action  = "store_true",
-         )),
-        (("-d", "--debug",),
-         "show the request and response in JSON", args(
-             action  = "store_true",
-         )),
-        (("-v", "--version",),
-         "show version", args(
-             action  = "version",
-             version = f"%(prog)s {version(package_name)}",
-         )),
-    ]
-
-    parser = argparse.ArgumentParser(
-        description="OpenAI GPT API command line wrapper",
-    )
-
-    for args, help_message, kwargs in spec:
-        if "default" in kwargs:
-            help_message += f" (default: {kwargs['default']})"
-        parser.add_argument(*args, help=help_message, **kwargs)
-
-    return parser
-
-def cli():
-
-    parser = create_parser()
-    args = parser.parse_args()
-
-    api_key = args.key or os.environ.get("OPENAI_API_KEY")
+@app.command()
+def main(
+    prompts:     List[str] = typer.Argument(...,                        help="Input prompts for GPT or '-' to read from stdin"),
+    system:      List[str] = typer.Option([], "--system", "-s",         help="Set system message (can be used multiple times)"),
+    itemize:     Optional[str] = typer.Option(None, "--itemize", "-I",  help="Itemize other prompts after this message"),
+    engine:      str = typer.Option("gpt-3.5-turbo", "--engine", "-e",  help="OpenAI GPT engine"),
+    max_tokens:  int = typer.Option(2000, "--max-tokens", "-m",         help="Maximum number of tokens in the response"),
+    temperature: float = typer.Option(0.5, "--temperature", "-t",       help="Sampling temperature for randomness"),
+    key:         Optional[str] = typer.Option(None, "--key", "-k",      help="OpenAI API key"),
+    squeeze:     bool = typer.Option(False, "--squeeze", "-q",          help="Squeeze two or more newlines into one"),
+    debug:       bool = typer.Option(False, "--debug", "-d",            help="Show the request and response in JSON"),
+    version:     bool = typer.Option(False, "--version", callback=vers, help="Show version")
+):
+    """
+    OpenAI GPT API command line wrapper.
+    """
+    api_key = key or os.environ.get("OPENAI_API_KEY")
     if api_key is None:
-        raise ValueError("Set OPENAI_API_KEY or use --key option.")
+        raise typer.Exit("Set OPENAI_API_KEY or use --key option.")
     openai.api_key = api_key
 
     prompt_parts = []
-    for p in args.prompts:
+    for p in prompts:
         if p == "-":
             prompt_parts.append(sys.stdin.read())
         else:
-            if args.itemize:
+            if itemize:
                 prompt_parts.append('* ' + p)
             else:
                 prompt_parts.append(p)
 
-    if args.itemize:
-        prompt_parts.insert(0, args.itemize)
+    if itemize:
+        prompt_parts.insert(0, itemize)
 
     prompt = "\n".join(prompt_parts)
 
-    if args.system:
-        messages = [ {"role": "system", "content": sys_message} for sys_message in args.system ]
+    if system:
+        messages = [{"role": "system", "content": sys_message} for sys_message in system]
     else:
-        messages = [
-            {"role": "system", "content": "You are a helpful assistant."},
-        ]
+        messages = [{"role": "system", "content": "You are a helpful assistant."}]
     messages.append({"role": "user", "content": prompt})
 
     request_params = {
-        "model": args.engine,
+        "model": engine,
         "messages": messages,
-        "max_tokens": args.max_tokens,
+        "max_tokens": max_tokens,
         "n": 1,
         "stop": None,
-        "temperature": args.temperature,
+        "temperature": temperature,
         "timeout": 60,
     }
 
-    if args.debug:
+    if debug:
         debug_print("\nRequest Parameters:")
         debug_print(json.dumps(request_params, indent=2, ensure_ascii=False))
 
     with click_spinner.spinner():
         response = openai.ChatCompletion.create(**request_params)
 
-    if args.debug:
+    if debug:
         debug_print("\nFull JSON Response:")
         debug_print(json.dumps(response, indent=2, ensure_ascii=False))
 
     generated_text = response.choices[0].message['content'].strip()
 
-    if args.squeeze:
+    if squeeze:
         generated_text = re.sub(r'\n\n+', '\n', generated_text)
 
     print(generated_text)
 
 if __name__ == "__main__":
-    cli()
+    app()
